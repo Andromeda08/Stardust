72f5b98e51f1583233b549774be9cd39098a0562
Andromeda08 <vkovacsbalazs26@gmail.com>
2022-10-14 02:02:40 +0200

    🔧 Simplified test vertex data.

--- Source/Core/Resources/Vertex.hpp
+++ Source/Core/Resources/Vertex.hpp
@@ -6,9 +6,8 @@
 
 struct Vertex
 {
-    glm::vec3 position;
+    glm::vec2 position;
     glm::vec3 color;
-    glm::vec3 normal;
     glm::vec2 uv;
 
     static vk::VertexInputBindingDescription binding_description()
@@ -22,36 +21,34 @@ struct Vertex
 
     static std::vector<vk::VertexInputAttributeDescription> attribute_description()
     {
-        std::vector<vk::VertexInputAttributeDescription> attrs(4);
+        std::vector<vk::VertexInputAttributeDescription> attrs(3);
 
-        // vec3 - [Position, Color, Normal]
-        for (size_t i = 0; i < 3; i++)
-        {
-            attrs[i].setBinding(0);
-            attrs[i].setLocation(i);
-            attrs[i].setFormat(vk::Format::eR32G32B32A32Sfloat);
-        }
+        attrs[0].setBinding(0);
+        attrs[0].setLocation(0);
+        attrs[0].setFormat(vk::Format::eR32G32Sfloat);
         attrs[0].setOffset(offsetof(Vertex, position));
+
+        attrs[1].setBinding(0);
+        attrs[1].setLocation(1);
+        attrs[1].setFormat(vk::Format::eR32G32B32Sfloat);
         attrs[1].setOffset(offsetof(Vertex, color));
-        attrs[2].setOffset(offsetof(Vertex, normal));
 
         // vec2 - [UV]
-        attrs[3].setBinding(0);
-        attrs[3].setLocation(3);
-        attrs[3].setFormat(vk::Format::eR32G32Sfloat);
-        attrs[3].setOffset(offsetof(Vertex, uv));
+        attrs[2].setBinding(0);
+        attrs[2].setLocation(2);
+        attrs[2].setFormat(vk::Format::eR32G32Sfloat);
+        attrs[2].setOffset(offsetof(Vertex, uv));
 
         return attrs;
     }
 };
 
 const std::vector<Vertex> test_vertices = {
-    {{-0.5f, 0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}, {0, 1, 0}, {1.0f, 0.0f}},
-    {{ 0.5f, 0.0f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0, 1, 0}, {0.0f, 0.0f}},
-    {{ 0.5f, 0.0f,  0.5f}, {0.0f, 0.0f, 1.0f}, {0, 1, 0}, {0.0f, 1.0f}},
-    {{-0.5f, 0.0f,  0.5f}, {1.0f, 1.0f, 1.0f}, {0, 1, 0}, {1.0f, 1.0f}}
+    {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+    {{ 0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
+    {{ 0.5f,  0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
 };
 
 const std::vector<uint32_t> test_indices = {
-    0, 1, 2, 2, 3, 0
+    0, 1, 2
 };
