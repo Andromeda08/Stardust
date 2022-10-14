d022d7b747134febb96d8ee023afe0b338ae1e72
Andromeda08 <vkovacsbalazs26@gmail.com>
2022-10-14 02:04:31 +0200

    ➕ CommandPools now support buffer resetting.

--- Source/Core/Vulkan/Command/CommandPool.cpp
+++ Source/Core/Vulkan/Command/CommandPool.cpp
@@ -6,6 +6,7 @@ CommandPool::CommandPool(const Device& device, uint32_t queue_family_index)
     vk::CommandPoolCreateInfo create_info;
     create_info.setQueueFamilyIndex(queue_family_index);
     create_info.setPNext(nullptr);
+    create_info.setFlags(vk::CommandPoolCreateFlagBits::eResetCommandBuffer);
 
     auto result = m_device.handle().createCommandPool(&create_info, nullptr, &m_handle);
 }
