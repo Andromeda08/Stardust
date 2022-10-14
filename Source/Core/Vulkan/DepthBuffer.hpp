343a25e12693f5bdca0306b2cbddced80ceb3d17
Andromeda08 <vkovacsbalazs26@gmail.com>
2022-10-15 00:02:33 +0200

    🔧 DepthBuffer now uses Image and ImageView classes to reduce boilerplate code.

--- Source/Core/Vulkan/DepthBuffer.hpp
+++ Source/Core/Vulkan/DepthBuffer.hpp
@@ -3,21 +3,32 @@
 #include <vulkan/vulkan.hpp>
 #include "Device.hpp"
 #include "Command/CommandBuffers.hpp"
+#include "Image/Image.hpp"
+#include "Image/ImageView.hpp"
+#include "../Macro.hpp"
 
 class DepthBuffer
 {
 public:
-    DepthBuffer(vk::Extent2D extent,
-                const Device& device,
+    NON_COPIABLE(DepthBuffer)
+
+    DepthBuffer(vk::Extent2D          extent,
+                const Device&         device,
                 const CommandBuffers& cmd_buffers);
 
     vk::Format format() const { return m_format; }
 
-    const vk::ImageView& view() const { return m_depth_view; }
+    const vk::ImageView& view() const { return m_depth_view->handle(); }
 
 private:
-    vk::Format       m_format;
-    vk::Image        m_depth_image;
-    vk::ImageView    m_depth_view;
-    vk::DeviceMemory m_depth_memory;
+    vk::Format find_depth_format();
+
+private:
+    std::unique_ptr<Image>     m_depth_image;
+
+    std::unique_ptr<ImageView> m_depth_view;
+
+    vk::Format m_format;
+
+    const Device& m_device;
 };
