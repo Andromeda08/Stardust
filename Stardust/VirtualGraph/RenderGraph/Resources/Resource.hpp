4f75a8f76097560091492d99cf60ba4a8ead7a98
Andromeda08 <vkovacsbalazs26@gmail.com>
2023-09-18 01:02:09 +0200

    Add missing resources.

--- Stardust/VirtualGraph/RenderGraph/Resources/Resource.hpp
+++ Stardust/VirtualGraph/RenderGraph/Resources/Resource.hpp
@@ -2,38 +2,62 @@
 
 #include <memory>
 #include <Nebula/Image.hpp>
+#include <Scene/Camera.hpp>
+#include <Scene/Object.hpp>
+#include <Vulkan/Raytracing/Tlas.hpp>
 
 namespace Nebula::RenderGraph
 {
-    class Resource
+    struct Resource
     {
-    public:
         virtual ~Resource() = default;
     };
 
-    class ImageResource : public Resource {
-    public:
-        ImageResource(const std::shared_ptr<Nebula::Image>& image)
-        : Resource()
-        , resource(image)
-        {}
+    struct CameraResource : public Resource
+    {
+        explicit CameraResource(const std::shared_ptr<sd::Camera>& camera)
+        : Resource(), m_camera(camera) {}
+
+        std::shared_ptr<sd::Camera> m_camera;
+    };
+
+    struct DepthImageResource : public Resource
+    {
+        explicit DepthImageResource(const std::shared_ptr<Nebula::Image>& depth_image)
+        : Resource(), resource(depth_image) {}
 
         std::shared_ptr<Nebula::Image> resource;
     };
 
-    class DepthImageResource : public Resource {
-    public:
-        DepthImageResource(const std::shared_ptr<Nebula::Image>& depth_image)
-        : Resource()
-        , resource(depth_image)
-        {}
+    struct ImageResource : public Resource
+    {
+        explicit ImageResource(const std::shared_ptr<Nebula::Image>& image)
+        : Resource(), resource(image) {}
 
         std::shared_ptr<Nebula::Image> resource;
     };
 
-    class CameraResource : public Resource {};
+    struct ImageArrayResource : public Resource
+    {
+        explicit ImageArrayResource(const std::vector<std::shared_ptr<Nebula::Image>>& images)
+        : Resource(), m_images(images) {}
 
-    class TlasResource : public Resource {};
+        std::vector<std::shared_ptr<Nebula::Image>> m_images;
+    };
 
-    class ObjectsResource : public Resource {};
+    struct ObjectsResource : public Resource
+    {
+        explicit ObjectsResource(const std::vector<sd::Object>& objects)
+        : Resource(), m_objects(objects) {}
+
+        const std::vector<sd::Object>& m_objects;
+    };
+
+    struct TlasResource : public Resource
+    {
+        explicit TlasResource(const std::shared_ptr<sdvk::Tlas>& tlas)
+        : Resource(), m_tlas(tlas) {}
+
+        std::shared_ptr<sdvk::Tlas> m_tlas;
+    };
 }
