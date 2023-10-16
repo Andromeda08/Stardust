23ffe4171d9f4648b6c901de038eb6ba98e30dc0
Andromeda08 <vkovacsbalazs26@gmail.com>
2023-10-16 22:24:22 +0200

    Change Buffer resource type color

--- Stardust/VirtualGraph/RenderGraph/Resources/ResourceType.cpp
+++ Stardust/VirtualGraph/RenderGraph/Resources/ResourceType.cpp
@@ -10,19 +10,19 @@ namespace Nebula::RenderGraph
         switch (type)
         {
             case ResourceType::eBuffer:
-                return { 210, 15, 57, 255 };
+                return { 251, 191, 36, 255 };
             case ResourceType::eCamera:
-                return { 234, 118, 203, 255 };
+                return { 236, 72, 153, 255 };
             case ResourceType::eDepthImage:
-                return { 210, 15, 57, 255 };
+                return { 20, 184, 166, 255 };
             case ResourceType::eImage:
-                return { 30, 102, 245, 255 };
+                return { 59, 130, 246, 255 };
             case ResourceType::eImageArray:
-                return { 136, 57, 239, 255 };
+                return { 14, 165, 233, 255 };
             case ResourceType::eObjects:
-                return { 223, 142, 29, 255 };
+                return { 244, 63, 94, 255 };
             case ResourceType::eTlas:
-                return { 64, 160, 43, 255 };
+                return { 217, 70, 239, 255 };
             case ResourceType::eUnknown:
             default:
                 return { 128, 128, 128, 255 };
