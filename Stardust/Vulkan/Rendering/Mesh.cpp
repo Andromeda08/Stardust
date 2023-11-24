c219948940e77c56465b771d6fcdd68ac2ead5a7
Andromeda08 <vkovacsbalazs26@gmail.com>
2023-11-24 14:10:06 +0100

    Add Meshlet generation

--- Stardust/Vulkan/Rendering/Mesh.cpp
+++ Stardust/Vulkan/Rendering/Mesh.cpp
@@ -6,13 +6,13 @@ namespace sdvk
     Mesh::Mesh(sd::Geometry* p_geometry, const CommandBuffers& command_buffers, const Context& context, const std::string& name)
     : m_geometry(p_geometry), m_name(name)
     {
-        m_vertex_buffer = sdvk::Buffer::Builder()
+        m_vertex_buffer = Buffer::Builder()
             .with_name(std::format("[Mesh] {} - Vertex Buffer", name))
             .with_size(sizeof(sd::VertexData) * m_geometry->vertices().size())
             .as_vertex_buffer()
             .create_with_data(m_geometry->vertices().data(), command_buffers, context);
 
-        m_index_buffer = sdvk::Buffer::Builder()
+        m_index_buffer = Buffer::Builder()
             .with_name(std::format("[Mesh] {} - Index Buffer", name))
             .with_size(sizeof(uint32_t) * m_geometry->indices().size())
             .as_index_buffer()
@@ -27,6 +27,14 @@ namespace sdvk
                 .with_name(std::format("[Mesh] {} - BLAS", name))
                 .create(command_buffers, context);
         }
+
+        create_meshlets();
+        m_meshlets_size = m_meshlets.size();
+        m_meshlet_buffer = Buffer::Builder()
+            .with_size(sizeof(Meshlet) * m_meshlets.size())
+            .as_storage_buffer()
+            .with_name(std::format("[Mesh] {} - Meshlets", name))
+            .create_with_data(m_meshlets.data(), command_buffers, context);
     }
 
     void Mesh::draw(const vk::CommandBuffer& command_buffer) const
@@ -35,5 +43,73 @@ namespace sdvk
         command_buffer.bindVertexBuffers(0, 1, &m_vertex_buffer->buffer(), offsets.data());
         command_buffer.bindIndexBuffer(m_index_buffer->buffer(), 0, vk::IndexType::eUint32);
         command_buffer.drawIndexed(m_geometry->indices().size(), 1, 0, 0, 0);
+    }
+
+    void Mesh::draw_mesh_tasks(const vk::CommandBuffer& command_buffer) const
+    {
+        command_buffer.drawMeshTasksEXT(m_meshlets_size, 1, 1);
+    }
+
+    void Mesh::create_meshlets(const uint32_t max_vertices, const uint32_t max_indices)
+    {
+        const auto& geom = *m_geometry;
+        const auto& indices = geom.indices();
+
+        std::vector<Meshlet> meshlets;
+        std::vector<uint8_t> meshlet_vertices(geom.vertex_count(), 0xff);
+        Meshlet meshlet = {};
+
+        for (uint32_t i = 0; i < geom.index_count(); i += 3)
+        {
+            const uint32_t a = indices[i + 0];
+            const uint32_t b = indices[i + 1];
+            const uint32_t c = indices[i + 2];
+
+            uint8_t& av = meshlet_vertices[a];
+            uint8_t& bv = meshlet_vertices[b];
+            uint8_t& cv = meshlet_vertices[c];
+
+            if (meshlet.vertex_count + (av == 0xff) + (bv == 0xff) + (cv == 0xff) > max_vertices
+                || meshlet.index_count + 3 > max_indices)
+            {
+                meshlets.push_back(meshlet);
+
+                for (uint32_t j = 0; j < meshlet.vertex_count; j++)
+                {
+                    meshlet_vertices[meshlet.vertex[j]] = 0xff;
+                }
+
+                meshlet = {};
+            }
+
+            if (av == 0xff)
+            {
+                av = meshlet.vertex_count;
+                meshlet.vertex[meshlet.vertex_count++] = a;
+            }
+
+            if (bv == 0xff)
+            {
+                bv = meshlet.vertex_count;
+                meshlet.vertex[meshlet.vertex_count++] = b;
+            }
+
+            if (cv == 0xff)
+            {
+                cv = meshlet.vertex_count;
+                meshlet.vertex[meshlet.vertex_count++] = c;
+            }
+
+            meshlet.index[meshlet.index_count++] = av;
+            meshlet.index[meshlet.index_count++] = bv;
+            meshlet.index[meshlet.index_count++] = cv;
+        }
+
+        if (meshlet.index_count > 0)
+        {
+            meshlets.push_back(meshlet);
+        }
+
+        m_meshlets = meshlets;
     }
 }
--- Stardust/Vulkan/Rendering/Mesh.hpp
+++ Stardust/Vulkan/Rendering/Mesh.hpp
@@ -7,16 +7,30 @@
 
 namespace sdvk
 {
+    struct Meshlet
+    {
+        uint32_t vertex[64]{};
+        uint32_t index[126]{};
+        uint32_t vertex_count {0};
+        uint32_t index_count {0};
+    };
+
     class Mesh
     {
     public:
-        Mesh(sd::Geometry* p_geometry, CommandBuffers const& command_buffers, Context const& context, std::string const& name = "");
+        Mesh(sd::Geometry* p_geometry, const CommandBuffers& command_buffers, const Context& context, const std::string& name = "");
 
-        void draw(vk::CommandBuffer const& command_buffer) const;
+        void draw(const vk::CommandBuffer& command_buffer) const;
+
+        void draw_mesh_tasks(const vk::CommandBuffer& command_buffer) const;
 
         const Buffer& vertex_buffer() const { return *m_vertex_buffer; }
 
         const Buffer& index_buffer() const { return *m_index_buffer; }
+
+        const Buffer& meshlet_buffer() const { return *m_meshlet_buffer; }
+
+        const std::vector<Meshlet>& meshlet_data() const { return m_meshlets; }
 
         const sd::Geometry& geometry() const { return *m_geometry; }
 
@@ -25,11 +39,15 @@ namespace sdvk
         const vk::DeviceAddress& blas_address() const { return m_blas->address(); }
 
     private:
-        std::string m_name;
+        void create_meshlets(uint32_t max_vertices = 64, uint32_t max_indices = 126);
 
+        std::string                   m_name;
+        uint32_t                      m_meshlets_size;
+        std::vector<Meshlet>          m_meshlets;
         std::shared_ptr<sd::Geometry> m_geometry;
-        std::shared_ptr<Buffer> m_vertex_buffer;
-        std::shared_ptr<Buffer> m_index_buffer;
-        std::unique_ptr<Blas> m_blas;
+        std::shared_ptr<Buffer>       m_vertex_buffer;
+        std::shared_ptr<Buffer>       m_index_buffer;
+        std::shared_ptr<Buffer>       m_meshlet_buffer;
+        std::unique_ptr<Blas>         m_blas;
     };
 }
